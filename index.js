3ea2136bf2187142572cd7b638e2a052c956e409
Katya Pavlenko <katya@lawgeex.com>
2020-03-28 18:42:01 +0300

    post request is done

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
-var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+var express = require('express')
+var graphqlHTTP = require('express-graphql')
+var { buildSchema } = require('graphql')
 
-const data = ['ff','ggg']
+const data = ['ff', 'ggg']
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
     getData: [String]
+    addItem(text: String): String
   }
-`);
+`)
 
 // The root provides a resolver function for each API endpoint
 var root = {
   getData: () => {
+    console.log(data)
     return data
   },
-};
+  addItem: ({text}) => {
+    console.log(text)
+    data.push(text)
+    return 'Done'
+  }
+}
 
-var app = express();
+var app = express()
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
-  graphiql: true,
-}));
+  graphiql: true
+}))
 
-app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+app.listen(4000)
+console.log('Running a GraphQL API server at http://localhost:4000/graphql')
