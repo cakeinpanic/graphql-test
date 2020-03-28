69cbd12aae1821e27a4b424d6686d6b0187bed5f
Katya Pavlenko <katya@lawgeex.com>
2020-03-28 18:59:55 +0300

    add more complex backend type

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ var express = require('express')
 var graphqlHTTP = require('express-graphql')
 var { buildSchema } = require('graphql')
 
-const data = ['ff', 'ggg']
+const data = [{id:'1', text:'ddd'}]
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
+  scalar Item {
+    id: String
+    text: String
+  }
   type Query {
-    getData: [String]
+    getData: [Item]
     addItem(text: String): String
   }
 `)
+
 
 // The root provides a resolver function for each API endpoint
 var root = {
@@ -18,8 +23,7 @@ var root = {
     return data
   },
   addItem: ({text}) => {
-    console.log(text)
-    data.push(text)
+    data.push({id: data.length.toString(), text})
     return 'Done'
   }
 }
